a198a5c19e74fb98ad619fca4d04bdd09bf1fc26
roncalloj <107959215+roncalloj@users.noreply.github.com>
2023-11-22 12:56:06 +0000

    Another exercise of copilot

--- webapp/main.py
+++ webapp/main.py
@@ -35,3 +35,23 @@ def generate(body: Body):
     """
     string = base64.b64encode(os.urandom(64))[:body.length].decode('utf-8')
     return {'token': string}
+
+# Add a Pydantic model
+
+
+class Text(BaseModel):
+    text: str
+
+# Create a FastAPI endpoint that accepts a POST request with a JSON body containing a single field called "text" and returns a checksum of the text.
+
+
+@app.post('/checksum')
+def checksum(text: Text):
+    """
+    Generate a checksum of the text. Example POST request body:
+
+    {
+        "text": "Hello World!"
+    }
+    """
+    return {'checksum': hash(text.text)}
